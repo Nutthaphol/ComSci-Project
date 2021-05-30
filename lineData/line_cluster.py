000eee0ab0d64cc30e201d867dcb1f3d86b6be86
Nutthaphol Dechpramualphol <manzun_nut18@hotmail.com>
2021-05-30 22:30:00 +0700

    cluster text from line message

---
+++ lineData/line_cluster.py
@@ -0,0 +1,31 @@
+import json, csv,codecs
+from pythainlp import sent_tokenize, word_tokenize
+
+
+with open('food_res.txt',"r") as reader:
+        texts = reader.readlines()
+        set_texts = []
+        check = True;
+        for text in texts:
+                if len(text) <= 1: 
+                        check = False
+                if (text.find("joined the group.") != -1):
+                        check = False
+                # add text
+                if (check):
+                        set_texts.append(text.split(' '))
+                check = True
+        
+        last_texts = []
+        for text in set_texts:
+                message = ""
+                for index in range(2,len(text)):
+                        message += text[index]
+                if message != "":
+                        last_texts.append(message)
+                        
+        
+        with codecs.open("food_data.csv","w", "utf-8") as write:
+                text_write = csv.writer(write)
+                for text in last_texts:
+                        text_write.writerow([text])
