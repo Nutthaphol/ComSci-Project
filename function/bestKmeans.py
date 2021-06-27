d22ad32f7ae1e93a782f5280bdd1d0b573106b62
Nutthaphol Dechpramualphol <manzun_nut18@hotmail.com>
2021-06-27 21:06:30 +0700

    done cluster 3 level

--- function/bestKmeans.py
+++ function/bestKmeans.py
@@ -4,7 +4,7 @@ from sklearn.metrics import silhouette_score
 def best_k (feature, max_):
     num_sil = []
     k = 0
-    maximum = 0
+    maximum = -1
     
     for i in range(2, max_+1):
         model = KMeans(n_clusters=i).fit(feature)
@@ -14,4 +14,5 @@ def best_k (feature, max_):
         if num_sil[-1] > maximum:
             maximum = num_sil[-1]
             k = i
+
     return k
