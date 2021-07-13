a12c53c6f6d5ecad9e00c8328f62ea4020b7b7ec
Nutthaphol Dechpramualphol <manzun_nut18@hotmail.com>
2021-07-13 18:18:11 +0700

    label

--- multi_clustering.py
+++ multi_clustering.py
@@ -4,10 +4,19 @@ from function.kmean import Kmean
 from function.dbscan import DBSCAN
 from function.sse import SSE
 from function.tf_idf import TF_IDF
+from pythainlp import  word_tokenize
+
 
 if __name__ == '__main__':
     df = pd.read_csv("lineData/comsci_data.csv") 
     text = df.text
+    text = df.text
+    clean_text = []
+    for sen in text:
+        words = word_tokenize(sen,engine='newmm')
+        stop_words = [i for i in words if i not in "\n"]
+        clean_text.append(''.join(i for i in stop_words))
+    df.text = clean_text
 
     fe = TF_IDF(text=text,format="thai") # create tf-idf format "thai"/"english"
     
@@ -36,13 +45,13 @@ if __name__ == '__main__':
                                                                     "dist_score":kmean_three_level["dist_score"],\
                                                                     "avg_dist":kmean_three_level["avg_dist_score"]})  # select the desired column
     kmean_three_level = kmean_three_level.sort_values(by=['centroids_id'])
-    kmean_three_level.to_csv("comsci_result/Kmean_three_level.csv",encoding='utf-8-sig')
-    sse_kmean_three_level.to_csv("comsci_result/SSE_kmean_three_level.csv",encoding='utf-8-sig')
+    kmean_three_level.to_csv("comsci_result/Kmean_three_level.csv",encoding='utf-8-sig',index=False)
+    sse_kmean_three_level.to_csv("comsci_result/SSE_kmean_three_level.csv",encoding='utf-8-sig',index=False)
 
     kmean_normal = kmean_normal.sort_values(by=["centroids_id"])
-    kmean_normal.to_csv("comsci_result/Kmean_normal.csv",encoding='utf-8-sig')
-    sse_kmean_normal.to_csv("comsci_result/SSE_kmean_normal.csv",encoding='utf-8-sig')
+    kmean_normal.to_csv("comsci_result/Kmean_normal.csv",encoding='utf-8-sig',index=False)
+    sse_kmean_normal.to_csv("comsci_result/SSE_kmean_normal.csv",encoding='utf-8-sig',index=False)
 
     dbscan = dbscan.sort_values(by=["centroids_id"])
-    dbscan.to_csv("comsci_result/DBSCAN.csv",encoding='utf-8-sig')
-    sse_dbscan.to_csv("comsci_result/SSE_DBSCAN.csv",encoding='utf-8-sig')
+    dbscan.to_csv("comsci_result/DBSCAN.csv",encoding='utf-8-sig',index=False)
+    sse_dbscan.to_csv("comsci_result/SSE_DBSCAN.csv",encoding='utf-8-sig',index=False)
---
+++ set_group_data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__": 
+        df = pd.read_csv("comsci_result/kmean_three_level.csv") # get data from keam 3 level
+
+        label_group = df.centroids_id.unique() # get label number
+        tmp_l = [0,1,2]
+        df["intent"] = np.NaN
+
+
+        for i in tmp_l: # insert label name by user
+                insert_label = df[df.centroids_id == i]
+                ex_text = insert_label[insert_label.dist_score == insert_label.dist_score.min()].text.sample(1).to_string(index=False)
+                print("message : ", ex_text)
+                label_name = input()
+                df.at[df.centroids_id == i, "intent"] = label_name
+        
+        label_name = df.intent.unique().tolist()
+        
+        for i in label_name: # save file with label name
+                data_group = df[df.intent == i].text
+                save_at = "data_training/"+str(i)+".csv"
+                data_group.to_csv(save_at,encoding='utf-8-sig',index=False)
+
+        
