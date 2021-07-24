1884addc65a4539c74aeb45822a4bbfbb0c9b432
Nutthaphol Dechpramualphol <manzun_nut18@hotmail.com>
2021-07-24 23:33:25 +0700

    test

--- clustering.py
+++ clustering.py
@@ -1,3 +1,4 @@
+from re import M
 from function.bestPCA import bestPCA
 from function.bestKmeans import best_k
 import pandas as pd
@@ -28,12 +29,14 @@ if __name__ == '__main__':
     
     kmean_normal = Kmean(fe=fe,data_= df.copy(),k_value=k_value)
     kmean_three_level = Kmean_three_level(fe=fe,data_= df.copy(),k_value=k_value)
-    dbscan = DBSCAN(fe=fe,data_= df.copy())
+    dbscan_3 = DBSCAN(fe=fe,data_= df.copy(), min_pts=3)
+    dbscan_5 = DBSCAN(fe=fe,data_= df.copy(), min_pts=5)
 
     # test sse function
     sse_kmean_normal = SSE(kmean_normal.copy())
     sse_kmean_three_level = SSE(kmean_three_level.copy())
-    sse_dbscan = SSE(dbscan.copy())
+    sse_dbscan_3 = SSE(dbscan_3.copy())
+    sse_dbscan_5 = SSE(dbscan_5.copy())
 
     ''' set show original '''
     # tm = kmean_three_level
@@ -53,18 +56,23 @@ if __name__ == '__main__':
     kmean_normal.to_csv("comsci_result/Kmean_normal.csv",encoding='utf-8-sig',index=False)
     sse_kmean_normal.to_csv("comsci_result/SSE_kmean_normal.csv",encoding='utf-8-sig',index=False)
 
-    dbscan = dbscan.sort_values(by=["centroids_id"])
-    dbscan.to_csv("comsci_result/DBSCAN.csv",encoding='utf-8-sig',index=False)
-    sse_dbscan.to_csv("comsci_result/SSE_DBSCAN.csv",encoding='utf-8-sig',index=False)
+    dbscan_3 = dbscan_3.sort_values(by=["centroids_id"])
+    dbscan_3.to_csv("comsci_result/DBSCAN_3.csv",encoding='utf-8-sig',index=False)
+    sse_dbscan_3.to_csv("comsci_result/SSE_DBSCAN_3.csv",encoding='utf-8-sig',index=False)
+
+    dbscan_5 = dbscan_5.sort_values(by=["centroids_id"])
+    dbscan_5.to_csv("comsci_result/DBSCAN_5.csv",encoding='utf-8-sig',index=False)
+    sse_dbscan_5.to_csv("comsci_result/SSE_DBSCAN_5.csv",encoding='utf-8-sig',index=False)
 
     sum_sse_kmean_normal = sse_kmean_normal["sse"].tolist()
     sum_sse_kmean_three_level = sse_kmean_three_level["sse"].tolist()
-    sum_sse_dbscan = sse_dbscan["sse"].tolist()
+    sum_sse_dbscan_3 = sse_dbscan_3["sse"].tolist()
+    sum_sse_dbscan_5 = sse_dbscan_5["sse"].tolist()
 
-    number_cluster = [len(sse_kmean_normal.centroids_id.tolist()), len(sse_kmean_three_level.centroids_id.tolist()), len(sse_dbscan.centroids_id.tolist())]
-    type = ["Kmean_normal","Kmean_three_level","dbscan"]
-    sum_sse = [sum(sum_sse_kmean_normal), sum(sum_sse_kmean_three_level), sum(sum_sse_dbscan)]
+    number_cluster = [len(sse_kmean_normal.centroids_id.tolist()), len(sse_kmean_three_level.centroids_id.tolist()), len(sse_dbscan_3.centroids_id.tolist()),len(sse_dbscan_3.centroids_id.tolist())]
+    type = ["Kmean_normal","Kmean_three_level","dbscan(minPts = 3)","dbscan(minPts = 5)"]
+    sum_sse = [sum(sum_sse_kmean_normal), sum(sum_sse_kmean_three_level), sum(sum_sse_dbscan_3), sum(sum_sse_dbscan_5)]
 
     conclusion = pd.DataFrame({"type":type, 'number':number_cluster, "sum_sse":sum_sse})
 
-    conclusion.to_csv("comsci_final.csv",encoding='utf-8-sig',index=False)
+    conclusion.to_csv("comsci_result/comsci_final.csv",encoding='utf-8-sig',index=False)
