d49b2667f423973da5f8fb09435f8865ac5081f5
Nutthaphol Dechpramualphol <manzun_nut18@hotmail.com>
2021-08-10 12:39:31 +0700

    add v dataset pca test

--- test_pca.py
+++ test_pca.py
@@ -22,33 +22,82 @@ if __name__ == '__main__':
 
     n_component = min(fe.shape[0], fe.shape[1])
 
+    pca_t = time.time()
     fe_pca = bestPCA(feature=fe, n_component=n_component)
+    print('pca time = ', time.time() - pca_t)
 
     k_value = best_k(feature=fe, max_=int(len(text)*0.1))
     k_value_pca = best_k(feature=fe_pca, max_=int(len(fe)*0.1))
 
-    eps_3 = bestEps(feature=fe)
-    eps_5 = bestEps(feature=fe)
-    eps_3_pca = bestEps(feature=fe_pca)
-    eps_5_pca = bestEps(feature=fe_pca)
+    eps = bestEps(feature=fe)
+    eps_pca = bestEps(feature=fe_pca)
     
     #kmean 
     print('kmean non pca')
+    kmeans_t = time.time()
     kmeans = Kmean(fe=fe, data_=df.copy(), k_value=k_value)
+    print('kmeans time = ', time.time() - kmeans_t)
     sse = sum(SSE(kmeans.copy()).sse)
     print('k value = ', k_value)
     print('sse = ', sse)
     print('shape = ', fe.shape)
-    kmeans = kmeans.sort_values(by=['centroids_id'])
-    print(kmeans.head(20))
     print('----------------------------')
+
     #kmean pca
     print('kmean pca')
+    kmeans_pca_t = time.time()
     kmeans = Kmean(fe=fe_pca, data_=df.copy(), k_value=k_value_pca)
+    print('kmeans with pca time = ', time.time() - kmeans_pca_t)
     sse = sum(SSE(kmeans.copy()).sse)
     print('k value = ', k_value_pca)
     print('sse = ', sse)
     print('shape = ', fe_pca.shape)
-    kmeans = kmeans.sort_values(by=['centroids_id'])
-    print(kmeans.head(20))
+    print('----------------------------')
+
+    #dbscan 3
+    print('dbscan minPts = 3')
+    db3_t = time.time()
+    dbscan_3 = DBSCAN(fe=fe, data_=df.copy(), eps_value=eps, min_pts=3)
+    print('DBSCAN minPts 3 time = ', time.time() - db3_t)
+    sse = sum(SSE(dbscan_3.copy()).sse)
+    print('eps value = ', eps)
+    print('sse = ', sse)
+    print('shape = ', fe.shape)
+    print('----------------------------')
+
+    #dbscan 5
+    print('dbscan minPts = 5')
+    db5_t = time.time()
+    dbscan_5 = DBSCAN(fe=fe, data_=df.copy(), eps_value=eps, min_pts=5)
+    print('dbscan minPts 5 time = ', time.time() - db5_t)
+    sse = sum(SSE(dbscan_5.copy()).sse)
+    print('eps value = ', eps)
+    print('sse = ', sse)
+    print('shape = ', fe.shape)
+    print('----------------------------')
+
+    #dbscan 3
+    print('dbscan minPts = 3 PCA')
+    db3_pca_t = time.time()
+    dbscan_3_pca = DBSCAN(fe=fe_pca, data_=df.copy(), eps_value=eps_pca, min_pts=3)
+    print('dbscan minPts 3 PCA time = ', time.time() - db3_pca_t)
+    sse = sum(SSE(dbscan_3.copy()).sse)
+    print('eps value = ', eps_pca)
+    print('sse = ', sse)
+    print('shape = ', fe_pca.shape)
+    print('----------------------------')
+
+    #dbscan 3
+    print('dbscan minPts = 5 PCA')
+    db5_pca_t = time.time()
+    dbscan_5_pca = DBSCAN(fe=fe_pca, data_=df.copy(), eps_value=eps_pca, min_pts=5)
+    print('dbscan minPts 5 PCA time = ', time.time() - db5_pca_t)
+    sse = sum(SSE(dbscan_5_pca.copy()).sse)
+    print('eps value = ', eps_pca)
+    print('sse = ', sse)
+    print('shape = ', fe_pca.shape)
+    print('----------------------------')
+
+    
+    
 
